8a0c7a45dfa0cf0662cd90eb92bd1fda97129319
Skittles9823 <skat1@live.com.au>
2018-07-17 19:06:58 +1000

    Change spinner text and switch to own CI channel

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
-#cp app/build/outputs/apk/release/app-release.apk TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk
-#curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001220326034" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-#curl -F chat_id="-1001386574981" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-#curl -F chat_id="-1001386574981" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+curl -F chat_id="-1001343279107" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+curl -F chat_id="-1001343279107" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
 echo $(./scripts/changelog.sh)
