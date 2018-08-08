c10cefeb25f85dba15164e725a63fc144a5e6029
Skittles9823 <skat1@live.com.au>
2018-08-08 16:05:32 +1000

    Only post playstore builds to the dev group

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 curl -F chat_id="-1001220326034" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001220326034" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001343279107" -F sticker="CAADBQADPQADTBCSGr060PvRNzIFAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
-curl -F chat_id="-1001343279107" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001343279107" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001220326034" -F document=@"TelegramXEmojis-Playstore-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
 echo $(./scripts/changelog.sh)
