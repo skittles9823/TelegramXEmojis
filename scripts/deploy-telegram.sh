699bd059d6e4e4adecb1133e78fa72555e9fbd09
Rhyse Simpson <skittles9832@gmail.com>
2019-02-21 17:03:04 +0100

    Lets see if this works

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
+upload(){
+curl -s -F 'file=@'"$1" "https://pixeldrain.com/api/file" >/dev/null 2>&1
+DOWNLOAD='https://pixeldrain.com/api/file/'$(curl -s -F 'file=@'"$1" "https://pixeldrain.com/api/file" | cut -d '"' -f 4)'?download'
+}
+upload TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001220326034" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001220326034" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="-1001220326034" -F text="$DOWNLOAD" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="-1001269732220" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001343279107" -F sticker="CAADBQADPQADTBCSGr060PvRNzIFAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
-curl -F chat_id="-1001343279107" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
-curl -F chat_id="-1001343279107" -F document=@"TelegramXEmojis-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-
-echo $(./scripts/changelog.sh)
+#curl -F chat_id="-1001343279107" -F sticker="CAADBQADPQADTBCSGr060PvRNzIFAg" https://api.telegram.org/bot$BOT_TOKEN/sendSticker
+#curl -F chat_id="-1001343279107" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+#curl -F chat_id="-1001343279107" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
